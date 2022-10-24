d642dac9afdb419ce1a94fa0583da4e8f3b9915e
Jimena B. Gonzalez <jibego2013@gmail.com>
2022-10-23 23:44:44 -0300

    Cambios 23/10 --> selection error

--- src/main/java/lyc/compiler/files/TercetoGenerator.java
+++ src/main/java/lyc/compiler/files/TercetoGenerator.java
@@ -4,10 +4,25 @@ import java.io.Serializable;
 import java.util.*;
 
 public class TercetoGenerator {
+    public static final Map<String, String> operadores = Map.of(
+            ">=", "BLT",
+            ">", "BLE",
+            "<=", "BGT",
+            "<", "BGE",
+            "<>", "BEQ",
+            "==", "BNE"
+    );
     public static Stack<Terceto> pilaTercetos = new Stack<Terceto>();
+    public static Stack<Integer> pilaIndices = new Stack<Integer>();
+    public static Stack<String> pilaOperadores = new Stack<String>();
     public static List<Terceto> tercetos = new LinkedList<Terceto>();
     public static Map<String, Integer> indices = new HashMap<String, Integer>();
+    /*
+    "ExpressionIndex" -> 2
+    2 -> Terceto [a,b,c]
+    * */
     private static TercetoGenerator instance;
+
     public static TercetoGenerator getInstance() {
         if (instance == null) {
             instance = new TercetoGenerator();
@@ -29,6 +44,11 @@ public class TercetoGenerator {
         System.out.println("VALUE DEL MAPA :" + indices.get(elementoNoTerminal).toString());
         return tercetos.size()-1;
     }
+    public int crearTerceto(Object o1, Object o2, Object o3) {
+        Terceto t = new Terceto(o1,o2,o3);
+        tercetos.add(t);
+        return tercetos.size()-1;
+    }
     //term ::= factor asignarTerceto("TermIndex", "FactorIndex")
     public void asignarTerceto(String ind1, String ind2) {
         indices.put(ind1,indices.get(ind2));
@@ -37,6 +57,10 @@ public class TercetoGenerator {
 
     public void apilarTerceto(String elementoNoTerminal) {
         pilaTercetos.push(tercetos.get(indices.get(elementoNoTerminal)));
+    }
+
+    public void apilarIndiceDeTerceto(String elementoNoTerminal) {
+        pilaIndices.push(indices.get(elementoNoTerminal));
     }
 
     public Object obtenerTercetoDelENT(String elementoNoTerminal) {
